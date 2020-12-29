bceb68e2209512a68fa80320e7178fe9eacbddfa
pawel-gawron <65308689+pawel-gawron@users.noreply.github.com>
2020-12-29 21:31:52 +0100

    create repository

---
+++ HelloPython.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 18 17:08:59 2020
+
+@author: ASUS_PEPE
+"""
+
+spam_amount = 0
+print(spam_amount)
+
+# Ordering Spam, egg, Spam, Spam, bacon and Spam (4 more servings of Spam)
+spam_amount = spam_amount + 4
+
+if spam_amount > 0:
+    print("But I don't want ANY spam!")
+
+viking_song = "Spam " * spam_amount
+print(viking_song)
